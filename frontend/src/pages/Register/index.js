bdc1aad6c5164b26fd1df1f1a469df87017c005c
Anya Parrille <lorran.parrilla@gmail.com>
2020-04-01 00:06:59 -0300

    criação do front de casos cadastrados

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles.css'
 import logoImg from '../../assets/logo.svg'
+
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 
